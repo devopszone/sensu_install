1762d4e83d224310ab38f1fab4cff89bc20980c9
devopszone <nunna.devops@gmail.com>
2018-06-28 17:14:21 +0530

    script to install sensu
    script to install sensu

---
+++ install_sensu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install git wget -y
+cd /tmp/
+wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -ivh epel-release-latest-7.noarch.rpm
+sudo yum install epel-release -y
+sudo yum install ansible -y
+cd /home/
+git clone https://github.com/devopszone/sensu_install.git
+cd /home/sensu_install
+/usr/bin/ansible-playbook setup.yml 
